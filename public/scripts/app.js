999adc704250c8902c688b9fea50ec6149a520e7
Vladyslav Kuliani <vladyslavkuliani@Vladyslavs-MacBook-Pro.local>
2016-11-29 12:21:52 -0800

    complete sprint 1

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,7 @@
 angular.module('tunely', []).controller('AlbumsIndexController', AlbumsIndexController);
 
-function AlbumsIndexController(){
+AlbumsIndexController.$inject = ['$http'];
+function AlbumsIndexController($http){
   var vm = this;
   vm.newAlbum = {};
 
@@ -9,18 +10,18 @@ function AlbumsIndexController(){
     artistName: 'Beastie Boys'
   };
 
-  vm.albums = [
-  {
-    name: 'Coming Home',
-    artistName: 'Leon Bridges'
-  },
-  {
-    name: 'Are We There',
-    artistName: 'Sharon Van Etten'
-  },
-  {
-    name: 'The Queen is Dead',
-    artistName: 'The Smiths'
+  vm.albums = [];
+
+  $http({
+    method: 'GET',
+    url: '/api/albums'
+  }).then(successGetAlbums, error);
+
+  function successGetAlbums(response){
+    vm.albums = response.data;
   }
-];
+
+  function error(err){
+    console.log(err);
+  }
 }
