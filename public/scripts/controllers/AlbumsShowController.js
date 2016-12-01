7c776a4873a8bffd75608c751f7861279062deff
Vladyslav Kuliani <vladyslavkuliani@Vladyslavs-MacBook-Pro.local>
2016-11-30 20:09:46 -0800

    final

--- public/scripts/controllers/AlbumsShowController.js
+++ public/scripts/controllers/AlbumsShowController.js
@@ -1,19 +1,4 @@
-<<<<<<< HEAD
-angular.module('tunely').controller('AlbumsShowController', AlbumsShowController);
 
-AlbumsShowController.$inject = ['$http', '$routeParams'];
-  function AlbumsShowController (  $http,   $routeParams  ) {
-    var vm = this;
-    console.log($routeParams);
-
-    $http({
-      method: 'GET',
-      url: '/api/albums/' + $routeParams.id// how can we get the id? (hint: check console log from above)
-    }).then(function successCallback(json) {
-      vm.album = json.data;
-    });
-  }
-=======
 angular
     .module('tunely')
     .controller('AlbumsShowController', AlbumsShowController);
